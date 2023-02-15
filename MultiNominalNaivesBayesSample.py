9c60dc6d2a585d9d19ef39f1cb5ea19dc182035d
AllanCKW <kw.allan.chong@gmail.com>
2023-02-16 01:53:23 +0800

    fix typo

--- MultiNominalNaivesBayesSample.py
+++ MultiNominalNaivesBayesSample.py
@@ -7,12 +7,9 @@ txtProcessorCSV_Train.preprocessText()
 
 txtProcessorCSV_Validate = TextPreprocessor_CSV('./data/validate.csv')
 txtProcessorCSV_Validate.preprocessText()
-#print(txtProcessorCSV.DataFrame.head())
-
-#print(txtProcessorCSV.DataFrame['text_lemmatized'].to_string(index=False))
 
 multinomialNaivesBayes_classifier = SklearnClassifier(MultinomialNB())
 
-multinomialNaivesBayes_classifier.train(txtProcessorCSV.all_tfidf_labeled_data)
+multinomialNaivesBayes_classifier.train(txtProcessorCSV_Train.all_tfidf_labeled_data)
        
 print(f'Accuracy of multinomial Naives Bayes classifier: {nltk.classify.accuracy(multinomialNaivesBayes_classifier, txtProcessorCSV_Validate.tfidf_labeled_data)}')
